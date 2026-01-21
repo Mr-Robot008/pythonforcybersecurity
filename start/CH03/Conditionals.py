ed5febfb227bde7c848e5488d1c91ed50a41d888
Mr-Robot008 <bezates.bryce@student.greenriver.edu>
2026-01-21 11:05:11 -0800

    conditionals

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # example workign with conditionals
-#By 
+#By Bryce Bezates on 1/21/2026
+
+# Ask the user for input
+answer = input("is today a good day? (y/n): ")
+
+# Check the answer
+if answer == "y":
+    print("yes it is")
+elif answer == "Y":
+    print("thats amazing!")
+if answer == "n":
+    print("hopefully it gets better")   
