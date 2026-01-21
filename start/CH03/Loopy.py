7f974e77a8f8533fbed81280717b583d66000e09
Mr-Robot008 <bezates.bryce@student.greenriver.edu>
2026-01-21 11:20:21 -0800

    Loop conditional script

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By Bryce bezates on 1/21/2026
+
+# Ask the user for input
+answer = input("is today a good day? (y/n): ")
+
+# Check the answer
+if answer == "y":
+    for i in range(10):
+        print("Yeah it is")
+elif answer == "Y":
+    print("thats amazing!")
+if answer == "n":
+    print("hopefully it gets better") 
