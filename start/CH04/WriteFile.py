de617bc2a5d768856186e175b40a009fe1b5642c
Mr-Robot008 <bezates.bryce@student.greenriver.edu>
2026-02-01 04:01:57 -0800

    saved information to hackme.txt file (Write HW)

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By Bryce Bezates 2/1/26
+
+name  = input("What is you name? ")
+color = input("What is your favorite color? ")
+pet = input("What was your first pet's name? ")
+mother_maiden = input("What is your mother's maiden name? ")
+school = input("What elementary school did you attend? ")
+
+with open("hackme.txt", "w") as file:
+    file.write("Name: " + name + "\n")
+    file.write("Favorite Color: " + color + "\n")
+    file.write("First Pet's Name: " + pet + "\n")
+    file.write("Mother's Maiden Name: " + mother_maiden + "\n")
+    file.write("Elementary School: " + school + "\n")
+
+print("Information saved to hackme.txt")
