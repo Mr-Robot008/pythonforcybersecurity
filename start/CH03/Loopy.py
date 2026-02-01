a748e2983ea24950dc17feef4fdec2acdded6ac3
Mr-Robot008 <bezates.bryce@student.greenriver.edu>
2026-02-01 04:38:14 -0800

    read hackme.txt file

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 # example workign with Loops
 #By Bryce bezates on 1/21/2026
+# Updated on 2/1/26
+
+def send_message():
+    for i in range(10):
+        print("Yeah it is")
 
 # Ask the user for input
 answer = input("is today a good day? (y/n): ")
 
 # Check the answer
 if answer == "y":
-    for i in range(10):
-        print("Yeah it is")
+    send_message()
 elif answer == "Y":
     print("thats amazing!")
 if answer == "n":
--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By Bryce Bezates 2/1/26
+
+
+with open("hackme.txt", "r") as file:
+    data = file.read()
+
+print("Here is someone to hack - information")
+
+print(data)
